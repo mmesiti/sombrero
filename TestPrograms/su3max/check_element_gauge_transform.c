cb2975ce2f1e79c0001fc84e62e48d5135a48e72
Michele Mesiti <michele.mesiti@gmail.com>
2020-09-17 17:22:30 +0100

    tests for elems transforms (gauge and spinors)

--- TestPrograms/su3max/check_element_gauge_transform.c
+++ TestPrograms/su3max/check_element_gauge_transform.c
@@ -1,8 +1,38 @@
-// empty template
-#include "maxeler.h"
+#include "LibHR/MaxelerInterface/maxeler/max_cg_API.h"
+#include "LibHR/MaxelerInterface/element_transforms/gauge_transforms.h"
+#include "su3_types.h"
+#include <assert.h>
+#include <complex.h>
 
 int main(){
 
+    {
+
+        suNg sombrero;
+        su3 maxeler;
+
+        {
+            int cr,cc;
+            for(cr=0;cr<3;++cr)
+                for(cc=0;cc<3;++cc){
+                   sombrero.c[cr*3+cc].re = cr*cc+10;
+                   sombrero.c[cr*3+cc].im = cr-cc;
+                }
+        }
+
+        sombrero_to_maxeler_gauge(&sombrero,&maxeler);
+
+        {
+            assert(maxeler.c00 == (0*0+10) + I*(0-0));
+            assert(maxeler.c01 == (0*1+10) + I*(0-1));
+
+            assert(maxeler.c10 == (1*0+10) + I*(1-0));
+            assert(maxeler.c11 == (1*1+10) + I*(1-1));
+
+            assert(maxeler.c20 == (2*0+10) + I*(2-0));
+            assert(maxeler.c21 == (2*1+10) + I*(2-1));
+        }
+    }
 
     return 0;
 
--- TestPrograms/su3max/check_element_spinor_transform.c
+++ TestPrograms/su3max/check_element_spinor_transform.c
@@ -1,7 +1,80 @@
-// empty template
-#include "maxeler.h"
+#include "LibHR/MaxelerInterface/maxeler/max_cg_API.h"
+#include "LibHR/MaxelerInterface/element_transforms/spinor_transforms.h"
+#include "su3_types.h"
+#include <assert.h>
+#include <complex.h>
 
 int main(){
+
+    {
+
+        suNg_spinor sombrero;
+        cg_spinor maxeler;
+
+        {
+            int d,c;
+            for(d=0;d<4;++d)
+                for(c=0;c<3;++c){
+                   sombrero.c[d].c[c].re = d*c+10;
+                   sombrero.c[d].c[c].im = c-d;
+                }
+        }
+
+        sombrero_to_maxeler_spinor(&sombrero,&maxeler);
+
+        assert(maxeler.s0c0 == (0*0+10)+ I*(0-0)) ;
+        assert(maxeler.s0c1 == (0*1+10)+ I*(1-0)) ;
+        assert(maxeler.s0c2 == (0*2+10)+ I*(2-0)) ;
+
+        assert(maxeler.s1c0 == (1*0+10)+ I*(0-1)) ;
+        assert(maxeler.s1c1 == (1*1+10)+ I*(1-1)) ;
+        assert(maxeler.s1c2 == (1*2+10)+ I*(2-1)) ;
+
+        assert(maxeler.s2c0 == (2*0+10)+ I*(0-2)) ;
+        assert(maxeler.s2c1 == (2*1+10)+ I*(1-2)) ;
+        assert(maxeler.s2c2 == (2*2+10)+ I*(2-2)) ;
+                                  
+        assert(maxeler.s3c0 == (3*0+10)+ I*(0-3)) ;
+        assert(maxeler.s3c1 == (3*1+10)+ I*(1-3)) ;
+        assert(maxeler.s3c2 == (3*2+10)+ I*(2-3)) ;
+
+    }
+
+
+    {
+
+        suNg_spinor sombrero;
+        cg_spinor maxeler;
+
+        maxeler.s0c0 = (0*0+10)+ I*(0-0) ;
+        maxeler.s0c1 = (0*1+10)+ I*(1-0) ;
+        maxeler.s0c2 = (0*2+10)+ I*(2-0) ;
+
+        maxeler.s1c0 = (1*0+10)+ I*(0-1) ;
+        maxeler.s1c1 = (1*1+10)+ I*(1-1) ;
+        maxeler.s1c2 = (1*2+10)+ I*(2-1) ;
+
+        maxeler.s2c0 = (2*0+10)+ I*(0-2) ;
+        maxeler.s2c1 = (2*1+10)+ I*(1-2) ;
+        maxeler.s2c2 = (2*2+10)+ I*(2-2) ;
+
+        maxeler.s3c0 = (3*0+10)+ I*(0-3) ;
+        maxeler.s3c1 = (3*1+10)+ I*(1-3) ;
+        maxeler.s3c2 = (3*2+10)+ I*(2-3) ;
+
+        maxeler_to_sombrero_spinor(&maxeler,&sombrero);
+
+        {
+            int d,c;
+            for(d=0;d<4;++d)
+                for(c=0;c<3;++c){
+                   assert(sombrero.c[d].c[c].re == d*c+10 );
+                   assert(sombrero.c[d].c[c].im == c-d );
+                }
+        }
+
+    }
+
 
 
     return 0;
